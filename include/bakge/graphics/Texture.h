af5d8af6f3f44a318bbf58641f626bc30b261f27
Julio Gutierrez <gut3717@yahoo.com>
2013-06-14 19:51:44 -0400

    Added code to Texture portion of Bakge

--- include/bakge/graphics/Texture.h
+++ include/bakge/graphics/Texture.h
@@ -22,9 +22,23 @@
  * THE SOFTWARE.
  * */
 
+
+/* *
+* Want to create this class? Here are some resources to get you
+* started with OpenGL texture creation.
+*
+* http://www.opengl.org/wiki/Texture
+* http://nehe.gamedev.net/tutorial/texture_mapping/12038/
+* http://www.swiftless.com/tutorials/opengl/texture_under_windows.html
+*
+* */
+
+
 #ifndef BAKGE_GRAPHICS_TEXTURE_H
 #define BAKGE_GRAPHICS_TEXTURE_H
 
+#include <gl/GL.h>
+#include <gl/GLU.h>
 #include <bakge/Bakge.h>
 
 namespace bakge
@@ -37,6 +51,8 @@ public:
 
     Texture();
     ~Texture();
+	Result Bind();
+	Result Unbind();
 
     /* *
      * Accepts metadata and raw image data to create an OpenGL texture
@@ -46,15 +62,17 @@ public:
     BGE_FACTORY Texture* Create(int Width, int Height, GLint Format,
                                             GLenum Type, void* Data);
 
-   /* *
-    * Want to create this class? Here are some resources to get you
-    * started with OpenGL texture creation.
-    *
-    * http://www.opengl.org/wiki/Texture
-    * http://nehe.gamedev.net/tutorial/texture_mapping/12038/
-    * http://www.swiftless.com/tutorials/opengl/texture_under_windows.html
-    *
-    * */
+	void Draw();
+
+private:
+	GLuint texture[1]; // The openGL texture ID
+	int width;
+	int height;
+	
+
+	
+
+
 
 }; /* Texture */
 
--- test/texture.cpp
+++ test/texture.cpp
@@ -7,17 +7,54 @@
 int main(int argc, char* argv[])
 {
     bakge::Texture* Tex;
+	bakge::Byte* pictureInfo;
+	bakge::Window* win;
 
     bakge::Init(argc, argv);
 
+	pictureInfo = bakge::LoadFileContents("C:\\Users\\Student\\Desktop\\texture.raw");
+
     /* When testing the completed class, pass raw image data as last param */
-    Tex = bakge::Texture::Create(64, 64, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    Tex = bakge::Texture::Create(64, 64, GL_RGB, GL_UNSIGNED_BYTE, (void*)pictureInfo);
+
+	win = bakge::Window::Create(600, 400);
+
+    bakge::Event Ev;
+
+    glClearColor(1, 0, 0, 1);
+
+
+	    while(1) {
+        while(win->PollEvent(&Ev) == BGE_SUCCESS) {
+            if(Ev.Type == -1)
+                win->Close();
+        }
+
+        /* Don't draw if the window has closed */
+        if(win->IsOpen() == false)
+            break;
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		Tex->Draw();
+
+        win->SwapBuffers();
+    }
+
+    
+
+
 
     /* *
      * Future texture testing will go here.
      * The texture will need to be drawn to fully test it, so a simple
      * window/engine class might be built for this test too
      * */
+
+
+
+	if(win != NULL)
+		delete win;
 
     if(Tex != NULL)
         delete Tex;
