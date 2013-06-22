59b4cd308b68fe5d9a703344058f930b5809c307
Paul Holden <paulholden2@gmail.com>
2013-06-22 13:36:42 -0700

    Flub with recent Window fix.

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -83,7 +83,7 @@ void Window::Key(GLFWwindow* Handle, int Key, int Code, int Pressed, int Mod)
     Handler = Win->Handler;
 
     if(Handler != NULL) {
-        /* Deploy event here */
+        Handler->KeyEvent(Key, Code, Pressed, Mod);
     }
 }
 
