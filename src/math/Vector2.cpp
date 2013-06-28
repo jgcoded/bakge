1e0dfef8b234b8e36b0b226411e10189bcff6834
Julio Gutierrez <jgutierrezleyva@mail.valenciacollege.edu>
2013-06-27 21:38:18 -0400

    Added constructors to Vector2

--- src/math/Vector2.cpp
+++ src/math/Vector2.cpp
@@ -1,42 +1,51 @@
 /* *
- * The MIT License (MIT)
- *
- * Copyright (c) 2013 Paul Holden et al. (See AUTHORS)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * */
- 
- #include <bakge/Bakge.h>
- 
- namespace bakge
- {
- namespace math
- {
- 
- Vector2::Vector2()
- {
- }
- 
- 
- Vector2::~Vector2()
- {
- }
- 
- } /* math */
- } /* bakge */
+* The MIT License (MIT)
+*
+* Copyright (c) 2013 Paul Holden et al. (See AUTHORS)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+* */
+
+#include <bakge/Bakge.h>
+
+namespace bakge
+{
+namespace math
+{
+
+Vector2::Vector2()
+{
+    Val[0] = 0;
+    Val[1] = 0;
+    Val[2] = 1.0;
+}
+
+Vector2::Vector2(Scalar X, Scalar Y, Scalar Z)
+{
+    Val[0] = X;
+    Val[1] = Y;
+    Val[2] = Z;
+}
+
+Vector2::~Vector2()
+{
+}
+
+} /* math */
+} /* bakge */
