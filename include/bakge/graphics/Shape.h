8517f352a8281d72851d99016167d9111a8ffeb8
Paul Holden <paulholden2@gmail.com>
2013-07-11 02:05:45 -0700

    Shape: Overload Pawn Bind/Unbind methods.

--- include/bakge/graphics/Shape.h
+++ include/bakge/graphics/Shape.h
@@ -51,6 +51,22 @@ public:
     Shape();
     ~Shape();
 
+    BGE_INL Result Bind() const
+    {
+        if(MeshVAO != 0)
+            glBindVertexArray(MeshVAO);
+        else
+            return BGE_FAILURE;
+
+        return Pawn::Bind();
+    }
+
+    BGE_INL Result Unbind() const
+    {
+        glBindVertexArray(0);
+        return Pawn::Unbind();
+    }
+
     Result DrawStyle(BGE_SHAPE_STYLE Style);
 
 }; /* Shape */
