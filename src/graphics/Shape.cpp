e3504fdf531de7fccf26e6275f17fe33f2806329
Paul Holden <paulholden2@gmail.com>
2013-07-15 00:47:51 -0700

    Shape: Now binding texcoord buffer.

--- src/graphics/Shape.cpp
+++ src/graphics/Shape.cpp
@@ -82,6 +82,10 @@ Result Shape::Bind() const
     glEnableVertexAttribArray(PositionsAttrib);
     glVertexAttribPointer(PositionsAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, MeshBuffers[MESH_BUFFER_TEXCOORDS]);
+    glEnableVertexAttribArray(TexCoordsAttrib);
+    glVertexAttribPointer(TexCoordsAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
     glBindBuffer(GL_ARRAY_BUFFER, MeshBuffers[MESH_BUFFER_NORMALS]);
     glEnableVertexAttribArray(NormalsAttrib);
     glVertexAttribPointer(NormalsAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
