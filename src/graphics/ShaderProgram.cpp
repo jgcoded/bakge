440367e1b935ba07657bcc90dfa75c9275c0884c
Paul Holden <paulholden2@gmail.com>
2013-07-16 22:53:59 -0700

    Shaderlib: Implementing bge_Model matrix.

--- src/graphics/ShaderProgram.cpp
+++ src/graphics/ShaderProgram.cpp
@@ -51,9 +51,15 @@ const char* bgeWorldTransformSource =
     "\n"
     "vec4 bgeWorldTransform()\n"
     "{\n"
+    "    mat4x4 bge_Model;\n"
+    "    bge_Model[0] = vec4(1, 0, 0, 0),\n"
+    "    bge_Model[1] = vec4(0, 1, 0, 0),\n"
+    "    bge_Model[2] = vec4(0, 0, 1, 0),\n"
+    "    bge_Model[3] = bge_Position;\n"
+    "\n"
     "    bge_TransformedNormal = (bge_Perspective * bge_View) * bge_Normal;\n"
     "    bge_TexCoord0 = bge_TexCoord;\n"
-    "    return (bge_Perspective * bge_View) * bge_Vertex;\n"
+    "    return (bge_Perspective * bge_View * bge_Model) * bge_Vertex;\n"
     "}\n"
     "\n";
 
