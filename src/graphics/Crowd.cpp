8adb2e37b194dce43c2be2b25cc26e5a509a8b4f
Paul Holden <paulholden2@gmail.com>
2013-08-09 23:25:29 -0700

    Crowd: No longer copy data when generating buffer

--- src/graphics/Crowd.cpp
+++ src/graphics/Crowd.cpp
@@ -58,16 +58,12 @@ Crowd* Crowd::Create(int ReserveMembers)
     C->NumMembers = ReserveMembers;
 
     /* *
-     * Generate `ReserveMembers` instances' model matrices
-     * Will use glBufferSubData in the future, to avoid generating an entire
-     * Matrix array
+     * Generate `ReserveMembers` instances' model matrices.
      * */
-    Matrix* Transformations = new Matrix[ReserveMembers];
     glBindBuffer(GL_ARRAY_BUFFER, C->CrowdBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Scalar) * 16 * ReserveMembers,
-                                    Transformations, GL_DYNAMIC_DRAW);
+                                    NULL, GL_DYNAMIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    delete[] Transformations;
 
     return C;
 }
