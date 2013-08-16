1f34dffda8f8201a38038079bc0905eacc22f2df
Paul Holden <paulholden2@gmail.com>
2013-08-16 16:22:44 -0700

    Build: Now use OpenAL 1.15.1

--- cmake/GetOpenAL.cmake
+++ cmake/GetOpenAL.cmake
@@ -1,7 +1,7 @@
 
-set(OPENAL_MIRROR http://connect.creativelabs.com/openal/Downloads/openal-soft-1.13.tbz2)
-set(OPENAL_TARGET ${BAKGE_SOURCE_DIR}/lib/openal-1.13)
-set(OPENAL_ARCHIVE ${OPENAL_TARGET}.tbz2)
+set(OPENAL_MIRROR http://kcat.strangesoft.net/openal-releases/openal-soft-1.15.1.tar.bz2)
+set(OPENAL_TARGET ${BAKGE_SOURCE_DIR}/lib/openal-1.15.1)
+set(OPENAL_ARCHIVE ${OPENAL_TARGET}.tar.bz2)
 
 if(NOT EXISTS ${OPENAL_ARCHIVE} AND NOT EXISTS ${OPENAL_TARGET})
   message("-- Downloading archive ${OPENAL_ARCHIVE}")
@@ -13,6 +13,7 @@ if(NOT EXISTS ${OPENAL_TARGET} AND EXISTS ${OPENAL_ARCHIVE})
   execute_process(COMMAND ${CMAKE_COMMAND} -E tar zxvf ${OPENAL_ARCHIVE} DEPENDS ${OPENAL_ARCHIVE} WORKING_DIRECTORY ${BAKGE_SOURCE_DIR}/lib OUTPUT_QUIET)
   message("-- Removing archive ${OPENAL_ARCHIVE}")
   file(REMOVE ${OPENAL_ARCHIVE})
-  file(RENAME ${BAKGE_SOURCE_DIR}/lib/openal-soft-1.13 ${OPENAL_TARGET})
+  file(RENAME ${BAKGE_SOURCE_DIR}/lib/openal-soft-1.15.1 ${OPENAL_TARGET})
 endif()
 
+#configure_file(${BAKGE_SOURCE_DIR}/cmake/BuildOpenAL.cmake ${OPENAL_TARGET}/CMakeLists.txt COPYONLY)
