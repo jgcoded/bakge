87cec851a3dc70ac8e5326219d02c8202869f275
Paul Holden <paulholden2@gmail.com>
2013-08-20 21:28:08 -0700

    Engine: Add documentation

--- include/bakge/core/Engine.h
+++ include/bakge/core/Engine.h
@@ -30,6 +30,12 @@
 namespace bakge
 {
 
+/*! @brief A framework interface for creating a game/application class
+ *
+ * This interface serves as a guide and framework when designing your
+ * engine class. By no means is it required, but many typical game engines
+ * will be implemented very similarly to this.
+ */
 class BGE_API Engine
 {
 
@@ -38,34 +44,54 @@ public:
     Engine();
     virtual ~Engine();
 
-    /* Initialize and clean up any components */
+    /*! @brief Initialize the Engine and its components
+     *
+     * Engines require starting. Implementations of this method should
+     * initialize all of the engine's components such as Renderers, UIs
+     * and scene managers.
+     */
     virtual Result Initialize() = 0;
+
+    /*! @brief Shut down the engine and do clean up
+     *
+     * Clean up code should be done in this method instead of the destructor.
+     */
     virtual Result ShutDown() = 0;
 
-    /* Run the application main loop */
+    /*! @brief Run the Engine, entering the main loop
+     *
+     * The Run method should house the engine's main loop, which updates the
+     * game world and renders it to the screen.
+     */
     virtual int Run() = 0;
 
-    /* *
-     * The main loop updates the application/game world here.
-     * Physics world steps, animation and anything that relates
-     * to time is done here.
-     * */
+    /*! @brief Update the game world with a time step of DeltaTime
+     *
+     * Time between Update calls should be measured and sent to the next
+     * call as the DeltaTime parameter. This keeps the world updated at
+     * a constant speed.
+     */
     virtual Result Update(Seconds DeltaTime) = 0;
 
-    /* *
-     * Rendering is split up into 3 stages, to allow for flexible
-     * rendering targets such as offscreen framebuffers or textures.
+    /*! @brief Prepare the engine for rendering
      *
-     * The PreRender stage is used to clear and set up framebuffers,
-     * set the viewing transform, etc.
-     *
-     * The Render stage is when scene and GUI rendering takes place.
-     *
-     * The PostRender stage usually does things like blitting framebuffers
-     * to the screen, or drawer/logic thread synchronization.
-     * */
+     * This method will typically clear framebuffers and set up state for
+     * rendering objects from the scene.
+     */
     virtual Result PreRenderStage() = 0;
+
+    /*! @brief Render the scene
+     *
+     * Render the scene's objects in this method.
+     */
     virtual Result RenderStage() = 0;
+
+    /*! @brief Run after the scene has been rendered
+     *
+     * This method will typically do things like blitting framebuffers,
+     * swapping window buffers or setting default OpenGL state to protect
+     * data from unintentional modification.
+     */
     virtual Result PostRenderStage() = 0;
 
 }; /* Engine */
