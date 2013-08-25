85521eefbd99cde9972fc6e99c952ce8c1df49ca
Paul Holden <paulholden2@gmail.com>
2013-08-25 02:30:16 -0700

    Matrix: Add documentation

--- include/bakge/math/Matrix.h
+++ include/bakge/math/Matrix.h
@@ -54,33 +54,102 @@ class BGE_API Matrix
 
 public:
 
+    /*! @brief Identity matrix.
+     *
+     * Use whenever you need an identity matrix instead of allocating a
+     * new one.
+     */
     static const Matrix Identity;
 
+    /*! @brief Default Matrix constructor.
+     *
+     * Default Matrix constructor.
+     */
     Matrix();
+
+    /*! @brief Matrix deconstructor.
+     *
+     * Matrix deconstructor.
+     */
     ~Matrix();
+
+    /*! @brief Matrix copy-constructor.
+     *
+     * Matrix copy-constructor.
+     */
     Matrix(Matrix BGE_NCP Other);
+
+    /*! @brief Matrix component constructor.
+     *
+     * Matrix component constructor.
+     *
+     * @warn May be removed or deprecated in the near future.
+     */
     Matrix(Scalar A, Scalar B, Scalar C, Scalar D,
            Scalar E, Scalar F, Scalar G, Scalar H,
            Scalar I, Scalar J, Scalar K, Scalar L,
            Scalar M, Scalar N, Scalar O, Scalar P);
+
+    /*! @brief Matrix column constructor.
+     *
+     * Construct a matrix with columns.
+     */
     Matrix(Vector4 BGE_NCP A, Vector4 BGE_NCP B, Vector4 BGE_NCP C,
                                                 Vector4 BGE_NCP D);
 
+    /*! @brief Matrix field accessor.
+     *
+     * Matrix field accessor.
+     *
+     * @param[in] At 0-base field index.
+     *
+     * @retval const reference to the Matrix.
+     */
     BGE_INL Scalar BGE_NCP operator[](int BGE_NCP At) const
     {
         return Val[At];
     }
 
-
+    /*! @brief Matrix field mutator.
+     *
+     * Matrix field mutator.
+     *
+     * @param[in] At 0-base field index.
+     *
+     * @retval Reference to field at specified index.
+     */
     BGE_INL Scalar& operator[](int BGE_NCP At)
     {
         return Val[At];
     }
 
+    /*! @brief Matrix assignment operator.
+     *
+     * Matrix assignment operator.
+     *
+     * @retval const reference to the Matrix.
+     */
     Matrix BGE_NCP operator=(Matrix BGE_NCP Other);
 
+    /*! @brief Matrix multiplication operator.
+     *
+     * Matrix multiplication operator.
+     *
+     * @param[in] Other Right operand.
+     *
+     * @retval Result matrix from multiplication of left and right operands.
+     */
     Matrix operator*(Matrix BGE_NCP Other) const;
 
+    /*! @brief Matrix multiplication compound operator.
+     *
+     * Matrix multiplication compound operator.
+     *
+     * @param[in] Other Right operand.
+     *
+     * @retval const reference to the matrix after compounded multiplication
+     * with right operand.
+     */
     Matrix BGE_NCP operator*=(Matrix BGE_NCP Other);
 
     Matrix BGE_NCP Invert();
