aed95b4999794ad16f7f203c04a3722a576477cd
Paul Holden <paulholden2@gmail.com>
2013-08-29 21:08:04 -0700

    Vector3: Operations affect homogeneous coord

--- src/math/Vector3.cpp
+++ src/math/Vector3.cpp
@@ -88,6 +88,7 @@ Vector3 BGE_NCP Vector3::operator+=(Vector3 BGE_NCP Other)
 {
     Val[0] += Other[0];
     Val[1] += Other[1];
+    Val[2] += Other[2];
 
     return *this;
 }
@@ -97,6 +98,7 @@ Vector3 BGE_NCP Vector3::operator-=(Vector3 BGE_NCP Other)
 {
     Val[0] -= Other[0];
     Val[1] -= Other[1];
+    Val[2] -= Other[2];
 
     return *this;
 }
@@ -106,6 +108,7 @@ Vector3 BGE_NCP Vector3::operator*=(Scalar BGE_NCP Value)
 {
     Val[0] /= Value;
     Val[1] /= Value;
+    Val[2] /= Value;
 
     return *this;
 }
@@ -119,6 +122,7 @@ Vector3 BGE_NCP Vector3::operator/=(Scalar BGE_NCP Value)
 
     Val[0] /= Value;
     Val[1] /= Value;
+    Val[2] /= Value;
 
     return *this;
 }
@@ -126,19 +130,19 @@ Vector3 BGE_NCP Vector3::operator/=(Scalar BGE_NCP Value)
 
 Vector3 Vector3::operator+(Vector3 BGE_NCP Other) const
 {
-    return Vector3(Val[0] + Other[0], Val[1] + Other[1], Val[2]);
+    return Vector3(Val[0] + Other[0], Val[1] + Other[1], Val[2] + Other[2]);
 }
 
 
 Vector3 Vector3::operator-(Vector3 BGE_NCP Other) const
 {
-    return Vector3(Val[0] - Other[0], Val[1] - Other[1], Val[2]);
+    return Vector3(Val[0] - Other[0], Val[1] - Other[1], Val[2] - Other[2]);
 }
 
 
 Vector3 Vector3::operator*(Scalar BGE_NCP Value) const
 {
-    return Vector3(Val[0] * Value, Val[1] * Value, Val[2]);
+    return Vector3(Val[0] * Value, Val[1] * Value, Val[2] * Value);
 }
 
 
@@ -148,7 +152,7 @@ Vector3 Vector3::operator/(Scalar BGE_NCP Value) const
         return *this;
     }
 
-    return Vector3(Val[0] / Value, Val[1] / Value, Val[2]);
+    return Vector3(Val[0] / Value, Val[1] / Value, Val[2] / Value);
 }
 
 
@@ -156,7 +160,7 @@ Vector3 Vector3::Normalized() const
 {
     Scalar Len = Length();
 
-    return Vector3(Val[0] / Len, Val[1] / Len, Val[2]);
+    return Vector3(Val[0] / Len, Val[1] / Len, Val[2] / Len);
 }
 
 
@@ -168,21 +172,21 @@ Scalar Vector3::Length() const
 
 Scalar Vector3::LengthSquared() const
 {
-    return Scalar(Val[0] * Val[0] + Val[1] * Val[1]);
+    return Scalar(Val[0] * Val[0] + Val[1] * Val[1] + Val[2] * Val[2]);
 }
 
 
 static Scalar Dot(Vector3 BGE_NCP Left, Vector3 BGE_NCP Right)
 {
-    return Left[0] * Right[0] + Left[1] * Right[1];
+    return Left[0] * Right[0] + Left[1] * Right[1] + Left[2] * Right[2];
 }
 
 
 static Vector3 UnitVector(Scalar BGE_NCP X, Scalar  BGE_NCP Y, Scalar BGE_NCP Z)
 {
-    Scalar Len = sqrt(X * X + Y * Y);
+    Scalar Len = sqrt(X * X + Y * Y + Z * Z);
 
-    return Vector3(X / Len, Y / Len, Z);
+    return Vector3(X / Len, Y / Len, Z / Len);
 }
 
 } /* bakge */
