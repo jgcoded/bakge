dfcfe1e07986eeb78bb2c9c7119326a9afe398c7
Paul Holden <paulholden2@gmail.com>
2013-09-01 09:45:55 -0700

    ShaderLibrary: Update documentation

--- include/bakge/graphics/ShaderLibrary.h
+++ include/bakge/graphics/ShaderLibrary.h
@@ -47,6 +47,33 @@
  */
 attribute mat4x4 bge_Model;
 
+/*! @brief Current viewing matrix.
+ *
+ * Set when a Camera class is bound.
+ */
+uniform mat4x4 bge_View;
+
+/*! @brief Current projection matrix.
+ *
+ * Set when a Camera class is bound.
+ */
+uniform mat4x4 bge_Perspective;
+
+/*! @brief Position of vertices being drawn in 3D Cartesian space.
+ *
+ * Even when drawing to 2D space the vertices are drawn in 3D space. Using a
+ * depth buffer and a Z value allows you to change the order of windows without
+ * having to use the painter's algorithm (draw objects back to front).
+ */
+attribute vec4 bge_Vertex;
+
+/*! @brief Normals of vertices in 3D Cartesian space.
+ *
+ * To get the transformed normals in eye space, multiply the normal by the
+ * 3x3 upper-left portion of the model-view matrix.
+ */
+attribute vec4 bge_Normal;
+
 /*! // End group VertexLib
  * @}
  */
