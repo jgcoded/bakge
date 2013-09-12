21d1605b079a092a018926d113e931961f811040
Paul Holden <paulholden2@gmail.com>
2013-09-12 15:59:31 -0700

    Mesh: Update documentation

--- include/bakge/graphics/Mesh.h
+++ include/bakge/graphics/Mesh.h
@@ -75,6 +75,16 @@ enum MESH_BUFFERS
     NUM_MESH_BUFFERS
 };
 
+/*! @brief A Mesh is a collection of vertex data describing an arbitrary object
+ *
+ * Meshes data and their associated metadata are used to describe objects
+ * to be drawn in 3D Cartesian space. These data include vertex positions,
+ * triangle indices, vertex normals and vertex texture coordinates.
+ *
+ * While these data and metadata are typically stored in various file formats,
+ * the Mesh class does not support loading or processing files. Instead it
+ * accepts raw vertex data and metadata through its various methods.
+ */
 class BGE_API Mesh : public Drawable
 {
 
@@ -86,16 +96,54 @@ protected:
 
     GLuint MeshBuffers[NUM_MESH_BUFFERS];
 
+    /*! @brief Default Mesh constructor.
+     *
+     * Default Mesh constructor.
+     */
     Mesh();
 
 
 public:
 
+    /*! @brief Virtual Mesh destructor.
+     *
+     * Virtual Mesh destructor.
+     */
     virtual ~Mesh();
 
+    /*! @brief Bind the mesh for drawing use.
+     *
+     * To draw a mesh it must first be bound. This sets OpenGL state so its
+     * vertex data is used in draw calls.
+     *
+     * @return BGE_SUCCESS if the Mesh was successfully bound; BGE_FAILURE
+     * if any errors occurred.
+     */
     virtual Result Bind() const;
+
+    /*! @brief Unbind the mesh after drawing use.
+     *
+     * Unbind sets associated OpenGL state to arbitrary defaults so the
+     * Mesh's vertex buffers or data remains safe from undesired modification.
+     *
+     * @return BGE_SUCCESS if the Mesh was successfully unbound; BGE_FAILURE
+     * if any errors occurred.
+     */
     virtual Result Unbind() const;
 
+    /*! @brief Instanced drawing used for rendering Crowds.
+     *
+     * This method is used to render a number of instances of a mesh in a
+     * single draw call. A Crowd must be bound before this call, otherwise
+     * behavior is undefined.
+     *
+     * @param[in] Count Number of instances in the Crowd to render.
+     *
+     * @return BGE_SUCCESS if rendering was successful; BGE_FAILURE if any
+     * errors occurred.
+     *
+     * @see bakge::Crowd
+     */
     virtual Result DrawInstanced(int Count) const;
 
 
