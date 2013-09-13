86b2356cbb82b92800c6a7941331ce003d2f8d58
Paul Holden <paulholden2@gmail.com>
2013-09-12 18:01:20 -0700

    GamePad: Add documentation

--- include/bakge/input/GamePad.h
+++ include/bakge/input/GamePad.h
@@ -22,6 +22,11 @@
  * THE SOFTWARE.
  * */
 
+/*!
+ * @file GamePad.h
+ * @brief GamePad class declaration.
+ */
+
 #ifndef BAKGE_INPUT_GAMEPAD_H
 #define BAKGE_INPUT_GAMEPAD_H
 
@@ -30,6 +35,13 @@
 namespace bakge
 {
 
+/*! @brief A joystick or controller used for player input.
+ *
+ * GamePads allow players to use a joystick or controller for game input.
+ * The API provides methods for getting raw axis values and button states.
+ * Currently GamePads do not trigger EventHandler events; you will have to
+ * implement your own way to detect changes in GamePad state.
+ */
 class GamePad
 {
     int Handle;
@@ -40,6 +52,10 @@ class GamePad
     int NumAxes;
     const Scalar* AxesValues;
 
+    /*! @brief Default GamePad constructor.
+     *
+     * Default GamePad constructor.
+     */
     GamePad();
 
 
@@ -47,26 +63,65 @@ public:
 
     static GamePad* AvailableGamePads;
 
+    /*! @brief Default GamePad destructor.
+     *
+     * Default GamePad destructor.
+     */
     ~GamePad();
 
+    /*! @brief Detects the first available joystick/controller and creates
+     * a GamePad instance if one is found.
+     *
+     * This factory method scans for any available joysticks or controllers
+     * and creates a new GamePad instance for the first available pad.
+     *
+     * @return Pointer to allocated GamePad or NULL if none are detected.
+     */
     BGE_FACTORY GamePad* Detect();
 
+    /*! @brief Get the number of buttons on the GamePad.
+     *
+     * Get the number of buttons on the GamePad.
+     *
+     * @return Number of buttons on the joystick/controller.
+     */
     BGE_INL int GetNumButtons() const
     {
         return NumButtons;
     }
 
+    /*! @brief Check if a GamePad button is pressed.
+     *
+     * Check if a GamePad button is pressed.
+     *
+     * @param[in] ButtonID The button's index on the GamePad.
+     *
+     * @return true if the button is pressed; false if it is not.
+     */
     BGE_INL bool GetButtonPressed(int ButtonID)
     {
         ButtonStates = glfwGetJoystickButtons(Handle, &NumButtons);
         return ButtonStates[ButtonID] == GLFW_PRESS;
     }
 
+    /*! @brief Get the number of axes on the GamePad.
+     *
+     * Get the number of axes on the GamePad.
+     *
+     * @return Number of axes on the joystick/controller.
+     */
     BGE_INL int GetNumAxes() const
     {
         return NumAxes;
     }
 
+    /*! @brief Get the value of an axis.
+     *
+     * The value of any given GamePad axis ranges from -1 to 1. The
+     * orientation may vary depending on the layout or type of GamePad.
+     *
+     * @return Floating-point value of the axis, ranging from -1.0 to 1.0.
+     */
     BGE_INL Scalar GetAxis(int AxisID)
     {
         AxesValues = glfwGetJoystickAxes(Handle, &NumAxes);
