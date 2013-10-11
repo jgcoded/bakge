734c0e2fd1cad988096515765a5f91163801bfc4
Paul Holden <paulholden2@gmail.com>
2013-10-10 18:25:04 -0700

    Mesh: Update log messages

--- src/graphics/Mesh.cpp
+++ src/graphics/Mesh.cpp
@@ -65,7 +65,7 @@ Result Mesh::Bind() const
 
 #ifdef DEBUG
     if(Program < 0) {
-        Log("Mesh: Unable to find current shader\n");
+        Log("ERROR: Mesh - Unable to find current shader\n");
         return BGE_FAILURE;
     }
 #endif /* _DEBUG */
@@ -118,22 +118,22 @@ Result Mesh::CreateBuffers()
 #ifdef _DEBUG
     /* Check to make sure each of our mesh's buffers was created properly */
     if(MeshBuffers[MESH_BUFFER_POSITIONS] == 0) {
-        Log("Mesh: Error creating positions buffer\n");
+        Log("ERROR: Mesh - Couldn't create positions buffer\n");
         return BGE_FAILURE;
     }
 
     if(MeshBuffers[MESH_BUFFER_NORMALS] == 0) {
-        Log("Mesh: Error creating normals buffer\n");
+        Log("ERROR: Mesh - Couldn't create normals buffer\n");
         return BGE_FAILURE;
     }
 
     if(MeshBuffers[MESH_BUFFER_TEXCOORDS] == 0) {
-        Log("Mesh: Error creating texture coordinates buffer\n");
+        Log("ERROR: Mesh - Couldn't create texture coordinates buffer\n");
         return BGE_FAILURE;
     }
 
     if(MeshBuffers[MESH_BUFFER_INDICES] == 0) {
-        Log("Mesh: Error creating triangle indices buffer\n");
+        Log("ERROR: Mesh - Couldn't create indices buffer\n");
         return BGE_FAILURE;
     }
 #endif /* _DEBUG */
