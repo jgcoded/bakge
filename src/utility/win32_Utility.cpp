8cd6e500177329442f6e8eb671011c5a9172dfdc
Paul Holden <paulholden2@gmail.com>
2013-10-26 17:20:43 -0700

    win32_Utility: SystemInfo implementation

--- src/utility/win32_Utility.cpp
+++ src/utility/win32_Utility.cpp
@@ -57,6 +57,144 @@ Result PlatformDeinit()
 
 void PlatformSystemInfo()
 {
+    OSVERSIONINFOEX Info;
+    SYSTEM_INFO Sys;
+
+    Info.dwOSVersionInfoSize = sizeof(Info);
+
+    GetVersionEx((LPOSVERSIONINFOA)&Info);
+    GetNativeSystemInfo(&Sys);
+
+    const char* VerString = NULL;
+
+    int Metric = -1;
+
+    if(Info.dwMajorVersion == 5) {
+        switch(Info.dwMinorVersion) {
+
+        case 0:
+            VerString = "Windows 2000";
+            break;
+
+        case 1:
+            VerString = "Windows XP";
+            break;
+
+        case 2:
+            if(Sys.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64 &&
+                                    Info.wProductType == VER_NT_WORKSTATION) {
+                VerString = "Windows XP Professional x64 Edition";
+                break;
+            }
+
+            if(Info.wSuiteMask & VER_SUITE_WH_SERVER) {
+                VerString = "Windows Home Server";
+                break;
+            }
+
+            Metric = GetSystemMetrics(SM_SERVERR2);
+            if(Metric != 0) {
+                VerString = "Windows Server 2003 R2";
+            } else {
+                VerString = "Windows Server 2003";
+            }
+
+            break;
+
+         default:
+            VerString = "Unknown";
+            break;
+        }
+    } else if(Info.dwMajorVersion == 6) {
+        switch(Info.dwMinorVersion) {
+
+        case 0:
+            if(Info.wProductType == VER_NT_WORKSTATION) {
+                VerString = "Windows Vista";
+            } else {
+                VerString = "Windows Server 2008";
+            }
+
+            break;
+
+        case 1:
+            if(Info.wProductType == VER_NT_WORKSTATION) {
+                VerString = "Windows 7";
+            } else {
+                VerString = "Windows Server 2008 R2";
+            }
+
+            break;
+
+        case 2:
+            if(Info.wProductType == VER_NT_WORKSTATION) {
+                VerString = "Windows 8";
+            } else {
+                VerString = "Windows Server 2012";
+            }
+
+            break;
+
+        case 3:
+            if(Info.wProductType == VER_NT_WORKSTATION) {
+                VerString = "Windows 8.1";
+            } else {
+                VerString = "Windows Server 2012 R2";
+            }
+
+            break;
+
+        default:
+            VerString = "Unknown";
+            break;
+        }
+    }
+
+    Log("====================\n");
+    Log("= Operating System =\n");
+    Log("====================\n");
+    Log("  %s\n", VerString);
+    Log("    - Build %d\n", Info.dwBuildNumber);
+    Log("    - PlatformID %d\n", Info.dwPlatformId);
+    Log("    - Service Pack %d.%d\n", Info.wServicePackMajor,
+                                        Info.wServicePackMinor);
+    Log("\n");
+
+    Log("===============\n");
+    Log("= System Info =\n");
+    Log("===============\n");
+
+    const char* ProcString = "i486";
+
+    switch(Sys.dwProcessorType) {
+
+    case PROCESSOR_INTEL_386:
+        ProcString = "i386";
+    case PROCESSOR_INTEL_486:
+        if((Sys.wProcessorRevision & 0xFFFF0000) == 0xFF0000) {
+            Log("  %s %d-%d\n", ProcString, (Sys.wProcessorRevision &
+                0x0000FF00) - 0xA, Sys.wProcessorRevision & 0x000000FF);
+        } else {
+            Log("  %s %d-%d\n", ProcString, (Sys.wProcessorRevision &
+               0xFFFF0000) + 'A', Sys.wProcessorRevision & 0x0000FFFF);
+        }
+
+        break;
+
+    case PROCESSOR_INTEL_PENTIUM:
+        Log("  Intel Pentium\n");
+        break;
+
+    case PROCESSOR_AMD_X8664:
+        Log("  AMD x86-64\n");
+        break;
+
+    default:
+        Log("  %d\n", Sys.dwProcessorType);
+        break;
+    }
+
+    Log("  %d processors\n", Sys.dwNumberOfProcessors);
 }
 
 } /* bakge */
