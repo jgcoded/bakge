896a4082d530f337ac362d158bbc5b4f28f58ed5
Paul Holden <paulholden2@gmail.com>
2013-10-28 00:40:50 -0700

    Add export_bmf_100.py script
    Blender addon for exporting Bakge mesh files of 1.0.0 specification. Note
    that the plugin version may greatly differ from the specification version.

---
+++ script/blender/export_bmf_100.py
@@ -0,0 +1,58 @@
+# The MIT License (MIT)
+# 
+# Copyright (c) 2013 Paul Holden et al. (See AUTHORS)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+bl_info = {
+    "name":         "Bakge Mesh File v1.0.0",
+    "author":       "Paul Holden",
+    "blender":      (2,6,2),
+    "version":      (1,0,0),
+    "location":     "File > Import-Export",
+    "description":  "Export Bakge Mesh File v1.0.0 specification.",
+    "category":     "Import-Export"
+}
+        
+import bpy
+from bpy_extras.io_utils import ExportHelper
+
+class ExportFormat(bpy.types.Operator, ExportHelper):
+    bl_idname       = "filename.bmf";
+    bl_label        = "Export BMF v1.0.0";
+    bl_options      = {'PRESET'};
+    
+    filename_ext    = ".bmf";
+    
+    def execute(self, context):
+        return {'Finished'};
+
+def menu_func(self, context):
+    self.layout.operator(ExportFormat.bl_idname, text="Bakge Mesh File v1.0.0 (.bmf)");
+
+def register():
+    bpy.utils.register_module(__name__);
+    bpy.types.INFO_MT_file_export.append(menu_func);
+    
+def unregister():
+    bpy.utils.unregister_module(__name__);
+    bpy.types.INFO_MT_file_export.remove(menu_func);
+
+if __name__ == "__main__":
+    register()
