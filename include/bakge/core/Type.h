7e35b208848b4058dc5c087f94c99a58b7960a78
Paul Holden <paulholden2@gmail.com>
2013-11-02 12:28:15 -0700

    Type.h: Add int16/uint16 typedefs

--- include/bakge/core/Type.h
+++ include/bakge/core/Type.h
@@ -42,11 +42,15 @@ typedef UINT64 uint64;
 typedef INT64 int64;
 typedef UINT32 uint32;
 typedef INT32 int32;
+typedef UINT16 uint16;
+typedef INT16 int16;
 #else
 typedef uint64_t uint64;
 typedef int64_t int64;
 typedef uint32_t uint32;
 typedef int32_t int32;
+typedef uint16_t uint16;
+typedef int16_t int16;
 #endif /* _WIN32 */
 /*! @endcond
  */
