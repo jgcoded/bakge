067450e22eeddc3d825b0c85adad844162fefb3e
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2013-11-02 21:53:39 -0400

    GetEnet: minor fixes

--- cmake/GetEnet.cmake
+++ cmake/GetEnet.cmake
@@ -1,10 +1,10 @@
 
 set(ENET_MIRROR http://enet.bespin.org/download/enet-1.3.10.tar.gz)
 set(ENET_TARGET ${BAKGE_SOURCE_DIR}/lib/enet-1.3.10)
-set(ENET_ARCHIVE ${ENET_TARGET}.tar.gz}
+set(ENET_ARCHIVE ${ENET_TARGET}.tar.gz)
 
 if(NOT EXISTS ${ENET_ARCHIVE} AND NOT EXISTS ${ENET_TARGET})
   message(STATUS "Enet library not found")
   message(STATUS "  Downloading archive ${ENET_ARCHIVE}")
-  file(DOWNLOAD ${ENET_MIRROR} ${LUA_ARCHIVE})
+  file(DOWNLOAD ${ENET_MIRROR} ${ENET_ARCHIVE})
 endif()
