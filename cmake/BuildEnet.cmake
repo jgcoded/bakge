7c9fe575a7aabb995100a5aa24940486fc4bedcb
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2013-11-02 22:22:30 -0400

    BuildEnet: add enet source files

--- cmake/BuildEnet.cmake
+++ cmake/BuildEnet.cmake
@@ -2,6 +2,20 @@
 
 if(ENET_TARGET)
 
+  include_directories(${ENET_TARGET})
+
+  set(ENET_SOURCES
+    ${ENET_TARGET}/callbacks.c
+    ${ENET_TARGET}/compress.c
+    ${ENET_TARGET}/host.c
+    ${ENET_TARGET}/list.c
+    ${ENET_TARGET}/packet.c
+    ${ENET_TARGET}/peer.c
+    ${ENET_TARGET}/protocol.c
+    ${ENET_TARGET}/unix.c 
+    ${ENET_TARGET}/win32.c
+  )
+
 else()
   message("Couldn't find Enet path")
 endif()
